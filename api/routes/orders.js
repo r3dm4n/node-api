ab089f873be14b2362890373e46ed55d7b153c91
Alexandru Corut <r3dm4n@me.com>
2020-08-08 12:56:17 +0300

    setup cors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,29 +2,35 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'orders get'
-    })
+  res.status(200).json({
+    message: 'orders get'
+  })
 })
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'handling post req to /products'
-    })
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  }
+
+  res.status(201).json({
+    message: 'handling post req to /orders',
+    order: order
+  })
 })
 
 router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'order get',
-        orderId: req.params.orderId
-    })
+  res.status(200).json({
+    message: 'order get',
+    orderId: req.params.orderId
+  })
 })
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'order deleted',
-        orderId: req.params.orderId
-    })
+  res.status(200).json({
+    message: 'order deleted',
+    orderId: req.params.orderId
+  })
 })
 
 module.exports = router
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,36 +2,42 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handling get req to /products'
-    })
+  res.status(200).json({
+    message: 'handling get req to /products'
+  })
 })
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'handling post req to /products'
-    })
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
+
+  res.status(201).json({
+    message: 'handling post req to /products',
+    createdProduct: product
+  })
 })
 
 router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    res.status(200).json({
-        message: id
-    })
+  const id = req.params.productId
+  res.status(200).json({
+    message: id
+  })
 })
 
 router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    res.status(200).json({
-        message: `Updated product: ${id}`
-    })
+  const id = req.params.productId
+  res.status(200).json({
+    message: `Updated product: ${id}`
+  })
 })
 
 router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    res.status(200).json({
-        message: `Deleted product: ${id}`
-    })
+  const id = req.params.productId
+  res.status(200).json({
+    message: `Deleted product: ${id}`
+  })
 })
 
 module.exports = router
